8802be29146c13bf29595df9192fe20bf04f5661
urban.flame <urban.fl4m3@gmail.com>
2021-05-12 14:08:17 +0300

    Added Chapter5_Singleton

---
+++ DesignPatterns/Chapter5_Singletone/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Chapter5_Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton.GetInstance().PrintHello();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DesignPatterns/Chapter5_Singletone/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Chapter5_Singleton
+{
+    public class Singleton
+    {
+        private Singleton()
+        {
+
+        }
+
+        public void PrintHello()
+        {
+            Console.WriteLine("Hello, world!");
+        }
+
+        private static Singleton _instance;
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+}
