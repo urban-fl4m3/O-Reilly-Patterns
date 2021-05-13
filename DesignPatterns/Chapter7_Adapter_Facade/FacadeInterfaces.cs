56fe0a0b2bfe408b5ee483f064bc3df045a2be7c
urban.flame <urban.fl4m3@gmail.com>
2021-05-13 10:14:57 +0300

    Added Facade pattern

---
+++ DesignPatterns/Chapter7_Adapter_Facade/FacadeInterfaces.cs
@@ -0,0 +1,74 @@
+﻿namespace Chapter7_Adapter_Facade
+{
+    public interface IAmplifier
+    {
+        void On();
+        void Off();
+        void SetCd(ICdPlayer cd);
+        void SetDvd(IDvdPlayer dvd);
+        void SetStereoSound();
+        void SetSurroundSound();
+        void SetTuner();
+        void SetVolume(int volume);
+    }
+
+    public interface IDvdPlayer
+    {
+        void On();
+        void Off();
+        void Eject();
+        void Pause();
+        void Play(string name);
+        void SetSurroundAudio();
+        void SetTwoChannelAudio();
+        void Stop();
+    }
+
+    public interface IProjector
+    {
+        void On();
+        void Off();
+        void TvMode();
+        void WideScreenMode();
+    }
+
+    public interface ICdPlayer
+    {
+
+        void On();
+        void Off();
+        void Eject();
+        void Pause();
+        void Play();
+        void Stop();
+    }
+
+    public interface ITheaterLights
+    {
+        void On();
+        void Off();
+        void Dim(int level);
+    }
+
+    public interface IScreen
+    {
+        void Up();
+        void Down();
+    }
+
+    public interface IPopcornPopper
+    {
+        void On();
+        void Off();
+        void Pop();
+    }
+
+    public interface ITunner
+    {
+        void On();
+        void Off();
+        void SetAm();
+        void SetFm();
+        void SetFrequency();
+    }
+}
---
+++ DesignPatterns/Chapter7_Adapter_Facade/HomeTheaterFacade.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Chapter7_Adapter_Facade
+{
+    public interface ITheaterFacade
+    {
+        void WatchMovie();
+        void EndMovide();
+    }
+
+    public class HomeTheaterFacade : ITheaterFacade
+    {
+        public IAmplifier Amp { get; }
+        public ITunner Tuner { get; }
+        public IDvdPlayer Dvd { get; }
+        public ICdPlayer Cd { get; }
+        public IProjector Projector { get; }
+        public ITheaterLights Lights { get; }
+        public IScreen Screen { get; }
+        public IPopcornPopper Popper { get; }
+
+        public HomeTheaterFacade(IAmplifier amp,
+            ITunner tuner,
+            IDvdPlayer dvd,
+            ICdPlayer cd,
+            IProjector projector,
+            ITheaterLights lights,
+            IScreen screen,
+            IPopcornPopper popper)
+        {
+            Amp = amp;
+            Tuner = tuner;
+            Dvd = dvd;
+            Cd = cd;
+            Projector = projector;
+            Lights = lights;
+            Screen = screen;
+            Popper = popper;
+        }
+
+        public void WatchMovie()
+        {
+            Console.WriteLine("Get ready to watch a movie...");
+            Popper.On();
+            Popper.Pop();
+            Lights.Dim(10);
+            Screen.Down();
+            Projector.On();
+            Projector.WideScreenMode();
+            Amp.On();
+            Amp.SetDvd(Dvd);
+            Amp.SetSurroundSound();
+            Amp.SetVolume(5);
+            Dvd.On();
+            Dvd.Play("Movie");
+        }
+
+        public void EndMovide()
+        {
+            Console.WriteLine("Shutting movie theater down...");
+            Popper.Off();
+            Lights.On();
+            Screen.Up();
+            Projector.Off();
+            Amp.Off();
+            Dvd.Stop();
+            Dvd.Eject();
+            Dvd.Off();
+        }
+    }
+}
